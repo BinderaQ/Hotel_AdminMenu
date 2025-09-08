0d8706ad129e1ba98e0979dfe4d1c61e9eee3e54
хліб <131171137+BinderaQ@users.noreply.github.com>
2025-09-08 19:52:56 +0300

    Base Commit

---
+++ testing/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+urlpatterns = [
+    path('',views.room_list, name='room_list'),
+    path('testing/create/', views.create_booking,name = 'create_booking'),
+    path('show/', views.show_calendar, name = 'show_calendar'),
+    path('my_bookings/', views.my_bookings, name='my_bookings'),
+    path('testing/<int:booking_id>/cancel',views.cancel, name = 'cancel'),
+    path('login/', auth_views.LoginView.as_view(template_name='testing/login.html'),name='login'),
+    path('logout/', auth_views.LogoutView.as_view(),name='logout'),
+]
